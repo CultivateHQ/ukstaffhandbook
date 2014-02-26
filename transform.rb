20dfd795b09408e86b6d32c314a0b4091dca6453
Paul Wilson <paulanthonywilson@gmail.com>
2014-02-26 16:25:59 +0000

    transferred over

--- transform.rb
+++ transform.rb
@@ -1,18 +1,26 @@
 require 'fileutils'
 
-Dir.glob("_posts/*.md") do |infile|
+FileUtils.mkdir_p "_includes"
 
-  infile =~ /\d{4}-\d{2}-\d{2}-(.+).md$/
-  outdir = "pages/#{$1}"
-  FileUtils.mkdir_p outdir
+File.open('_includes/toc.html', 'w') do |toc|
+  Dir.glob("_posts/*.md") do |infile|
 
-  outfile = "#{outdir}/index.md"
+    infile =~ /\d{4}-\d{2}-\d{2}-(.+).md$/
+    outdir = "pages/#{$1}"
+    FileUtils.mkdir_p outdir
+
+    outfile = "#{outdir}/index.md"
 
 
-  File.open(outfile, 'w') do |o|
-    File.open(infile) do |f|
-      f.each_line do |l|
-        o.write(l.gsub(/^layout:\s+post/, "layout: default"))
+
+    File.open(outfile, 'w') do |o|
+      File.open(infile) do |f|
+        f.each_line do |l|
+          o.write(l.gsub(/^layout:\s+post/, "layout: default"))
+          if l =~ /^title:\s*(.*)$/
+            toc.write "<li><a href='#{outdir}'>#{$1}</a></li>\n"
+          end
+        end
       end
     end
   end
